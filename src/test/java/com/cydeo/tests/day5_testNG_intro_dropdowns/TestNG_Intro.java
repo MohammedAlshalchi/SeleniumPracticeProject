25ef7ada21b167bd2da35a93a0bddd256bc06978
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-15 16:00:47 -0500

    commit3

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,11 +1,41 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import org.testng.annotations.*;
 
 
 public class TestNG_Intro {
+
+
+    @Test (priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+
+        //ASSERT EQUALS: compare 2 of the same things
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test (priority = 2)
+    public void test2() {
+        System.out.println("Test 2 is running...");
+
+        //AssertTrue
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertTrue(actual.equals(expected), "your message will go here");
+    }
+
+
+
+
+
     @BeforeClass
     public void setupMethod(){
         System.out.println("-----> BeforeClass is running!");
@@ -28,13 +58,17 @@ public class TestNG_Intro {
         System.out.println("---> AfterMethod is running!");
     }
 
-    @Test (priority = 1)
-    public void test1(){
-        System.out.println("Test 1 is running...");
-    }
 
-    @Test (priority = 2)
-    public void test2(){
-        System.out.println("Test 2 is running...");
-    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
